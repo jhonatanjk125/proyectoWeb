39c017bcd10b199dc70b8b3d41ed0cf67c78954c
jhonatanjk125 <jhonatanjk125@gmail.com>
2024-03-13 03:07:36 -0500

    feat: :construction: Close menu once a link is clicked on mobile

--- js/main.js
+++ js/main.js
@@ -2,9 +2,29 @@ function toggleMenu(){
     const menuToggle = document.querySelector('.toggle');
     const navigation = document.querySelector('.navigation');
     const logo = document.querySelector('#logo');
-    const header = document.querySelector('header');
-    menuToggle.classList.toggle('active')
-    navigation.classList.toggle('active')
-    logo.classList.toggle('active')
-    header.classList.toggle('active')
+    var mediaQuery = window.matchMedia("(max-width: 900px)");
+    if (mediaQuery.matches) {
+      menuToggle.classList.toggle('active')
+      navigation.classList.toggle('active')
+      logo.classList.toggle('active')
+    }
+    else {
+      return
+    }
+
   }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
