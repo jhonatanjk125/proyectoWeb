cbad50dc33dc45a822ff4c769634b28fa9c97490
jhonatanjk125 <jhonatanjk125@gmail.com>
2024-03-13 02:13:14 -0500

    feat: :construction: Fix responsive menu details to match the given schema

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 function toggleMenu(){
     const menuToggle = document.querySelector('.toggle');
     const navigation = document.querySelector('.navigation');
+    const logo = document.querySelector('#logo');
+    const header = document.querySelector('header');
     menuToggle.classList.toggle('active')
     navigation.classList.toggle('active')
+    logo.classList.toggle('active')
+    header.classList.toggle('active')
   }
