6443419b29103d34b5107b1dda688b9effb603c6
jhonatanjk125 <jhonatanjk125@gmail.com>
2024-03-14 17:54:12 -0500

    feat: :construction: Finish setting up dark-mode functionality in both mobile and desktop versions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 function toggleMenu() {
   const menuToggle = document.querySelector(".toggle");
   const navigation = document.querySelector(".navigation");
+  const darkModeToggle = document.getElementById("dark-mode-toggle");
+  darkModeToggle.classList.toggle("active");
   menuToggle.classList.toggle("active");
   navigation.classList.toggle("active");
 }
+
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+  document.documentElement.classList.toggle('dark-theme');
+  if (document.documentElement.classList.contains('dark-theme')) {
+    document.getElementById('dark-mode-toggle').classList.remove('bx-moon');
+    document.getElementById('dark-mode-toggle').classList.add('bx-sun');
+  } else {
+    document.getElementById('dark-mode-toggle').classList.remove('bx-sun');
+    document.getElementById('dark-mode-toggle').classList.add('bx-moon');
+  }
+});
