33535f4a449b7eacbdb54facbfc3303139ce86f7
jhonatanjk125 <jhonatanjk125@gmail.com>
2024-03-14 18:10:08 -0500

    fix: :bug: Fix a bug where the dark mode toggle was not working on desktop version

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 function toggleMenu() {
   const menuToggle = document.querySelector(".toggle");
   const navigation = document.querySelector(".navigation");
-  const darkModeToggle = document.getElementById("dark-mode-toggle");
+  const darkModeToggle = document.querySelector('.dark-mode-toggle');
   darkModeToggle.classList.toggle("active");
   menuToggle.classList.toggle("active");
   navigation.classList.toggle("active");
 }
 
-document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+function toggleDarkMode() {
+  console.log('executed')
   document.documentElement.classList.toggle('dark-theme');
   if (document.documentElement.classList.contains('dark-theme')) {
-    document.getElementById('dark-mode-toggle').classList.remove('bx-moon');
-    document.getElementById('dark-mode-toggle').classList.add('bx-sun');
+    document.querySelector('.dark-mode-toggle').classList.remove('bx-moon');
+    document.querySelector('.dark-mode-toggle').classList.add('bx-sun');
   } else {
-    document.getElementById('dark-mode-toggle').classList.remove('bx-sun');
-    document.getElementById('dark-mode-toggle').classList.add('bx-moon');
+    document.querySelector('.dark-mode-toggle').classList.remove('bx-sun');
+    document.querySelector('.dark-mode-toggle').classList.add('bx-moon');
   }
-});
+}
