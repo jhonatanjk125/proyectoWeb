114354227522f42eb3c6c89b6fcae626e4cf37d9
jhonatanjk125 <jhonatanjk125@gmail.com>
2024-03-15 00:27:35 -0500

    feat: :sparkles: :recycle: :construction: Maintain user preference when switching pages, prevent scrolling when opening mobile menu

--- js/main.js
+++ js/main.js
@@ -1,20 +1,51 @@
 function toggleMenu() {
   const menuToggle = document.querySelector(".toggle");
   const navigation = document.querySelector(".navigation");
-  const darkModeToggle = document.querySelector('.dark-mode-toggle');
+  const darkModeToggle = document.querySelector(".dark-mode-toggle");
   darkModeToggle.classList.toggle("active");
   menuToggle.classList.toggle("active");
   navigation.classList.toggle("active");
-}
-
-function toggleDarkMode() {
-  console.log('executed')
-  document.documentElement.classList.toggle('dark-theme');
-  if (document.documentElement.classList.contains('dark-theme')) {
-    document.querySelector('.dark-mode-toggle').classList.remove('bx-moon');
-    document.querySelector('.dark-mode-toggle').classList.add('bx-sun');
+  if (menuToggle.classList.contains("active")) {
+    document.body.style.overflow = 'hidden';
   } else {
-    document.querySelector('.dark-mode-toggle').classList.remove('bx-sun');
-    document.querySelector('.dark-mode-toggle').classList.add('bx-moon');
+    document.body.style.overflow = 'auto';
   }
 }
+
+function toggleDarkMode(element) {
+  const htmlElement = document.documentElement;
+  htmlElement.classList.toggle("dark-theme");
+  const darkModeToggles = document.querySelectorAll(".dark-mode-toggle");
+  if (htmlElement.classList.contains("dark-theme")) {
+    darkModeToggles.forEach(function (darkModeToggle) {
+      darkModeToggle.classList.replace("bx-moon", "bx-sun");
+    });
+  } else {
+    darkModeToggles.forEach(function (darkModeToggle) {
+      darkModeToggle.classList.replace("bx-sun", "bx-moon");
+    });
+  }
+  // Store the user preference in localStorage
+  const darkModeEnabled = htmlElement.classList.contains("dark-theme");
+  localStorage.setItem("darkMode", darkModeEnabled);
+}
+// Function to check and apply user preference on page load
+function applyUserPreference() {
+  const darkModeEnabled = localStorage.getItem('darkMode') === 'true';
+  const htmlElement = document.documentElement;
+  const darkModeToggles = document.querySelectorAll(".dark-mode-toggle");
+  if (darkModeEnabled) {
+      htmlElement.classList.add('dark-theme');
+      darkModeToggles.forEach(function (darkModeToggle) {
+        darkModeToggle.classList.replace("bx-moon", "bx-sun");
+      });
+
+  } else {
+      htmlElement.classList.remove('dark-theme');
+  }
+}
+
+// Call applyUserPreference function on page load
+document.addEventListener('DOMContentLoaded', function() {
+  applyUserPreference();
+});
